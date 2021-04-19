167113cb9632ccc94b772db8f2982cbe64703af9
Nathan <ntstew@gmail.com>
2021-04-19 12:34:06 -0400

    Update API URL

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -1,30 +1,22 @@
 import axios from "axios"
 
-const api = process.env.APIURL;
+const api = "https://api.sr.nathanstewart.me";
 
 export default {
     async getVerbConjugation(verb) {
         let res = await axios.get(api + "/c/" + verb);
-        console.log('Reach');
-        console.log(api);
         return res.data;
     },
     async getLanguage(text) {
         let res = await axios.get(api + "/l/" + text);
-        console.log('Reach');
-        console.log(api);
         return res.data;
     },
     async translateText(code, text) {
         let res = await axios.get(api + "/t/" + code + "/" + text);
-        console.log('Reach');
-        console.log(api);
         return res.data;
     },
     async isValidVerb(text) {
         let res = await axios.get(api + "/v/" + text);
-        console.log('Reach');
-        console.log(api);
         return res.data === true;
     }
 }
