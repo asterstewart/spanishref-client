c4e008e880b528461aa816758652f74d67105346
Nathan <ntstew@gmail.com>
2021-04-19 12:23:31 -0400

    More Debugging

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -5,19 +5,26 @@ const api = process.env.APIURL;
 export default {
     async getVerbConjugation(verb) {
         let res = await axios.get(api + "/c/" + verb);
+        console.log('Reach');
         console.log(api);
         return res.data;
     },
     async getLanguage(text) {
         let res = await axios.get(api + "/l/" + text);
+        console.log('Reach');
+        console.log(api);
         return res.data;
     },
     async translateText(code, text) {
         let res = await axios.get(api + "/t/" + code + "/" + text);
+        console.log('Reach');
+        console.log(api);
         return res.data;
     },
     async isValidVerb(text) {
         let res = await axios.get(api + "/v/" + text);
+        console.log('Reach');
+        console.log(api);
         return res.data === true;
     }
 }
