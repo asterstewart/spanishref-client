d577518b13b46a98f6815d1318e804531444f05d
Nathan <ntstew@gmail.com>
2021-04-19 15:00:31 -0400

    CORS is frustrating

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -4,19 +4,19 @@ const api = "https://api.sr.nathanstewart.me";
 
 export default {
     async getVerbConjugation(verb) {
-        let res = await axios.get(api + "/c/" + verb);
+        let res = await axios.get(api + "/c/" + verb + "/");
         return res.data;
     },
     async getLanguage(text) {
-        let res = await axios.get(api + "/l/" + text);
+        let res = await axios.get(api + "/l/" + text + "/");
         return res.data;
     },
     async translateText(code, text) {
-        let res = await axios.get(api + "/t/" + code + "/" + text);
+        let res = await axios.get(api + "/t/" + code + "/" + text + "/");
         return res.data;
     },
     async isValidVerb(text) {
-        let res = await axios.get(api + "/v/" + text);
+        let res = await axios.get(api + "/v/" + text + "/");
         return res.data === true;
     }
 }
