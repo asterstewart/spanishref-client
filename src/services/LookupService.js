58576cd3f64ab9653731d0691847359d18381574
Nathan <ntstew@gmail.com>
2021-04-20 12:39:51 -0400

    Auth

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -1,30 +1,34 @@
 import axios from "axios"
 
 const api = "https://api.sr.nathanstewart.me";
-
+const key = process.env.APIKEY;
 export default {
     async getVerbConjugation(verb) {
         let res = await axios.post(api + "/c", {
-            verb: verb
+            verb: verb,
+            key: key
         });
         return res.data;
     },
     async getLanguage(text) {
         let res = await axios.post(api + "/l", {
-            text: text
+            text: text,
+            key: key
         });
         return res.data;
     },
     async translateText(code, text) {
         let res = await axios.post(api + "/t", {
             origin: code,
-            text: text
+            text: text,
+            key: key
         });
         return res.data;
     },
     async isValidVerb(text) {
         let res = await axios.post(api + "/v", {
-            verb: text
+            verb: text,
+            key: key
         });
         return res.data === true;
     }
