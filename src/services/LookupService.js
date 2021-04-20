6d3a0be74f613ce21345dd92206f3e073718cbfa
Nathan Stewart <ntstew@gmail.com>
2021-04-20 19:39:18 -0400

    Removed key

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -1,40 +1,28 @@
 import axios from "axios"
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const secretClient = new SecretManagerServiceClient();
-const [version] = secretClient.accessSecretVersion({
-    name: 'projects/674145050597/secrets/spanishreference_api_key/versions/1'
-});
-const key = version.payload.data.toString();
-
 const api = "https://api.sr.nathanstewart.me";
 export default {
     async getVerbConjugation(verb) {
-        console.log(key);
         let res = await axios.post(api + "/c", {
-            verb: verb,
-            key: key
+            verb: verb
         });
         return res.data;
     },
     async getLanguage(text) {
         let res = await axios.post(api + "/l", {
-            text: text,
-            key: key
+            text: text
         });
         return res.data;
     },
     async translateText(code, text) {
         let res = await axios.post(api + "/t", {
             origin: code,
-            text: text,
-            key: key
+            text: text
         });
         return res.data;
     },
     async isValidVerb(text) {
         let res = await axios.post(api + "/v", {
-            verb: text,
-            key: key
+            verb: text
         });
         return res.data === true;
     }
