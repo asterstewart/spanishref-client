23f90b18791c2d60691cd84d68139f8749eaa237
Nathan Stewart <ntstew@gmail.com>
2021-04-20 00:12:11 -0400

    Switched to POST

--- src/services/LookupService.js
+++ src/services/LookupService.js
@@ -4,19 +4,28 @@ const api = "https://api.sr.nathanstewart.me";
 
 export default {
     async getVerbConjugation(verb) {
-        let res = await axios.get(api + "/c/" + verb + "/");
+        let res = await axios.post(api + "/c", {
+            verb: verb
+        });
         return res.data;
     },
     async getLanguage(text) {
-        let res = await axios.get(api + "/l/" + text + "/");
+        let res = await axios.post(api + "/l", {
+            text: text
+        });
         return res.data;
     },
     async translateText(code, text) {
-        let res = await axios.get(api + "/t/" + code + "/" + text + "/");
+        let res = await axios.post(api + "/t", {
+            origin: code,
+            text: text
+        });
         return res.data;
     },
     async isValidVerb(text) {
-        let res = await axios.get(api + "/v/" + text + "/");
+        let res = await axios.post(api + "/v", {
+            verb: text
+        });
         return res.data === true;
     }
 }
